0912084cc8688b4915dfd5d9baad95fed2e16294
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-19 18:07:59 +0200

    edit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+galleryList.addEventListener("click", onClick);
+
+const key = [];
+galleryItems.map((item) => {
+  key.push(
+    `<li class="gallery__item"><img class="gallery__image" data-big-img="${item.original}" src="${item.preview}" alt="${item.description}"></li>`
+  );
+});
+galleryList.insertAdjacentHTML("afterbegin", key.join(""));
+
+function onClick(event) {
+//   if (event.target !== event.currentTarget) {
+//     const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.bigImg}" width="800" height="600">`);
+
+//     instance.show();
+
+//     window.addEventListener("keydown", closeModal);
+//     const modal = document.querySelector(".basicLightbox");
+//     modal.addEventListener("click", closeModal);
+
+    // function closeModal(event) {
+    //   if (event.code === "Escape" || event.currentTarget === modal) {
+    //     window.removeEventListener("keydown", closeModal);
+    //     modal.removeEventListener("click", closeModal);
+    //     instance.close();
+    //   }
+    // }
+//   }
+}
