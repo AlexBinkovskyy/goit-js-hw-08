2fa4505beb0471cd0af2c4e1c5e38eb5c15cd86f
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-20 21:08:39 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,25 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css"
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryList = document.querySelector(".gallery");
-galleryList.addEventListener("click", onClick);
+const galleryList = document.querySelector('.gallery');
+galleryList.addEventListener('click', onClick);
 
 const key = [];
-galleryItems.map((item) => {
+galleryItems.map(item => {
   key.push(
-    `<li class="gallery__item"><img class="gallery__image" data-big-img="${item.original}" src="${item.preview}" alt="${item.description}"></li>`
+    `<li class="gallery"> <a href="${item.original}"><img src="${item.preview}" alt="" title="${item.description}"/></a>`
   );
 });
-galleryList.insertAdjacentHTML("afterbegin", key.join(""));
+galleryList.insertAdjacentHTML('afterbegin', key.join(''));
 
 function onClick(event) {
-//   if (event.target !== event.currentTarget) {
-//     const instance = basicLightbox.create(`
-//     <img src="${event.target.dataset.bigImg}" width="800" height="600">`);
-
-//     instance.show();
-
-//     window.addEventListener("keydown", closeModal);
-//     const modal = document.querySelector(".basicLightbox");
-//     modal.addEventListener("click", closeModal);
-
-    // function closeModal(event) {
-    //   if (event.code === "Escape" || event.currentTarget === modal) {
-    //     window.removeEventListener("keydown", closeModal);
-    //     modal.removeEventListener("click", closeModal);
-    //     instance.close();
-    //   }
-    // }
-//   }
+  if (event.target === event.currentTarget) {
+    event.preventDefault();
+    return;
+  }
 }
+
+const gallery = new SimpleLightbox('.gallery a');
