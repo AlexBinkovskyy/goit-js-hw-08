8d817ab798f68cc742ee7512c3e3bdf90e1b0453
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-20 22:13:25 +0200

    edited

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('playing', function(timeupdate) {
+        console.log(timeupdate);
+        localStorage.setItem("timeCode:")
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    // const onPlay = function(timeupdate) {
+    //     console.log(timeupdate);
+    //     // data is an object containing properties specific to that event
+    // };
+    
+    // player.on('play', onPlay);
+    
+    
+    // player.on('timeupdate').then(function(seconds) {
+    //     console.log(seconds);
+    // })
