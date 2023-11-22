109cbb94f2250f2a16beeaeac5f0259e3ac83ec8
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-23 00:03:08 +0200

    edited

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,36 +6,37 @@ form.addEventListener('submit', onSubmit);
 const submitBtn = form.lastElementChild;
 submitBtn.addEventListener('submit', onSubmit);
 
-const checkValues = JSON.parse(localStorage.getItem('feedback-form-state')) ?? '';
+const checkValues =
+  JSON.parse(localStorage.getItem('feedback-form-state')) ?? '';
 const emailInput = form.querySelector('[name="email"]');
 const messageInput = form.querySelector('[name="message"]');
 
 emailInput.value = checkValues.email ?? '';
 messageInput.value = checkValues.message ?? '';
 
-const throttleFunc = throttle(
-	500,
-	(feedbackFormState) => {
-		localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
-	}
-);
-
+const throttleFunc = throttle(500, feedbackFormState => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+});
 
 function inputValue(event) {
-    event.preventDefault();
-    const { elements : {email, message}} = event.currentTarget;
-    let feedbackFormState = {
-        'email': email.value,
-        'message': message.value
-    };
-    throttleFunc(feedbackFormState);
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  let feedbackFormState = {
+    email: email.value,
+    message: message.value,
+  };
+  throttleFunc(feedbackFormState);
 }
 
-
 function onSubmit(event) {
-    event.preventDefault();
-    console.log('email:', emailInput.value);
-    console.log('message:', messageInput.value);
-    form.reset();
-    localStorage.removeItem('feedback-form-state')
+  event.preventDefault();
+  console.log('email:', emailInput.value);
+  console.log('message:', messageInput.value);
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
 }
