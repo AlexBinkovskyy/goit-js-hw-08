39f5227462a88c1da7994430b5e7922f0109a05a
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-22 23:20:14 +0200

    allmost done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import _ from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', inputValue);
+form.addEventListener('submit', onSubmit);
+const submitBtn = form.lastElementChild;
+submitBtn.addEventListener('submit', onSubmit);
+
+const checkValues = JSON.parse(localStorage.getItem('feedback-form-state')) ?? '';
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+emailInput.value = checkValues.email ?? '';
+messageInput.value = checkValues.message ?? '';
+
+function inputValue(event) {
+    event.preventDefault();
+    const { elements : {email, message}} = event.currentTarget;
+    feedbackFormState = {
+        'email': email.value,
+        'message': message.value
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormState));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log('email:', emailInput.value);
+    console.log('message:', messageInput.value);
+    form.reset();
+    localStorage.removeItem('feedback-form-state')
+}
